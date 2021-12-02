40963c702d50d9da165312b1fb6f7361333b9094
Niels Tanis <nielstanis@live.nl>
2021-12-02 19:36:38 +0100

    t

---
+++ commit.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+in-toto-record start --verbose --step commit --key ../ndcoslo2021/in-toto/owner/owner
+git commit -S
+in-toto-record stop --verbose --step commit --products * --key ../ndcoslo2021/in-toto/owner/owner
