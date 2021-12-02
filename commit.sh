a848d6c062cba8893b2787b631c5cee8f6328fb6
Niels Tanis <nielstanis@live.nl>
2021-12-02 22:45:16 +0100

    change

--- commit.sh
+++ commit.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 in-toto-record start --verbose --step commit --key ../ndcoslo2021/in-toto/owner/owner
-git commit -S
+git commit -m "change" -S
 in-toto-record stop --verbose --step commit --products * --key ../ndcoslo2021/in-toto/owner/owner
